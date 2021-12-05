01630d4d9046365c3911837f81d88685e3d2b40a
Johnsonchan105 <johnsonchan105@gmail.com>
2021-12-05 03:46:49 -0800

    finished AOC Day 2

--- AOCDay2.py
+++ AOCDay2.py
@@ -1,21 +1,29 @@
 def main():
     with open('AOCDay2Inputs.txt', 'r') as f:
         movement = [line.strip() for line in f]
+    testMovement = ["forward 5", "down 5",
+                    "forward 8", "up 3", "down 8", "forward 2"]
     h, d = calcMove(movement)
     print(h * d)
+    #testH, testD = calcMove(testMovement)
+    #print(testH * testD)
 
 
 def calcMove(m):
     hPos = 0
     dPos = 0
-    for i in range(0, len(m) - 1):
+    aim = 0
+    for i in range(0, len(m)):
         move, distance = getMovement(m[i])
         if(move == 'f'):
             hPos += int(distance)
+            dPos += int(distance) * aim
         if(move == 'd'):
-            dPos += int(distance)
+            #dPos += int(distance)
+            aim += int(distance)
         if(move == 'u'):
-            dPos -= int(distance)
+            #dPos -= int(distance)
+            aim -= int(distance)
     return hPos, dPos
 
 
